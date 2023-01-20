d74b13f0b6510c3c6bef86941adced5731b79e83
Junayd H <junaydhus@gmail.com>
2023-01-19 16:21:20 -0800

    minor changes

--- breedFetcher.js
+++ breedFetcher.js
@@ -16,13 +16,13 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
     if (error) {
-      callback(error);
+      callback(error, null);
     } else {
       if (JSON.parse(body).length !== 0) {
         const data = JSON.parse(body);
         callback(null, (data[0].description));
       } else {
-        console.log('ERROR: Breed not found!');
+        callback('ERROR: Breed not found!', null);
       }
     }
   });
